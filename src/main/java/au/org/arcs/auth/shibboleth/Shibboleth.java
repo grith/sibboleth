66b9a728d049365b308a1a79f86f051d871fa07d
Markus Binsteiner <markus.binsteiner@arcs.org.au>
2009-09-17 11:21:27 +1000

    Fixes for the auto-dependency management.

--- src/main/java/au/org/arcs/auth/shibboleth/Shibboleth.java
+++ src/main/java/au/org/arcs/auth/shibboleth/Shibboleth.java
@@ -1,5 +1,6 @@
 package au.org.arcs.auth.shibboleth;
 
+import java.security.Security;
 import java.util.Iterator;
 
 import org.python.core.PyInstance;
@@ -9,60 +10,62 @@ import org.python.util.PythonInterpreter;
 public class Shibboleth {
 
 	private final ShibbolethClient shibClient;
-	
+
 	private final String url;
-	
-    public Shibboleth(String url) {
-    	this.url = url;
-        PythonInterpreter interpreter = new PythonInterpreter();
-        interpreter.exec("from arcs.shibboleth.client.shibboleth import Shibboleth");
-        PyObject shibbolethClientClass = interpreter.get("Shibboleth");
-        PyObject shibObject = shibbolethClientClass.__call__();
-        shibClient = (ShibbolethClient)shibObject.__tojava__(ShibbolethClient.class);
-    }
 
-    public PyInstance shibOpen(String username, char[] password, IdpObject idp) {
-    	
-    	return shibClient.shibopen(url, username, new String(password), idp);
-    	
-    }
+	public Shibboleth(String url) {
 
-    public PyInstance open() {
-    	
-    	return shibClient.open(url);
-    }
-	
-    
-    public static void main(String[] args) {
-    	
-    	java.security.Security.addProvider(new ArcsSecurityProvider());
+		this.url = url;
+		PythonInterpreter interpreter = new PythonInterpreter();
+		interpreter
+				.exec("from arcs.shibboleth.client.shibboleth import Shibboleth");
+		PyObject shibbolethClientClass = interpreter.get("Shibboleth");
+		PyObject shibObject = shibbolethClientClass.__call__();
+		shibClient = (ShibbolethClient) shibObject
+				.__tojava__(ShibbolethClient.class);
+	}
 
-    	java.security.Security.setProperty("ssl.TrustManagerFactory.algorithm", "TrustAllCertificates");
-    	
-    	Shibboleth shib = new Shibboleth("https://slcs1.arcs.org.au/SLCS/login");
-    	
-    	PyInstance returnValue = shib.shibOpen(args[0], args[1].toCharArray(), new DummyIdpObject());
-    	
-    	Iterable<PyObject> it = returnValue.asIterable();
-    	
-    	for ( Iterator i = it.iterator(); i.hasNext(); ) {
-    		
-    		System.out.println(i.next());
-    		
-    	}
-    	
-    	returnValue = shib.open();
-    	
-    	it = returnValue.asIterable();
-    	
-    	for ( Iterator i = it.iterator(); i.hasNext(); ) {
-    		
-    		System.out.println(i.next());
-    		
-    	}
-    	
-    	
-    }
-	
+	public PyInstance shibOpen(String username, char[] password, IdpObject idp) {
+
+		return shibClient.shibopen(url, username, new String(password), idp);
+
+	}
+
+	public PyInstance open() {
+
+		return shibClient.open(url);
+	}
+
+	public static void main(String[] args) {
+
+		java.security.Security.addProvider(new ArcsSecurityProvider());
+
+		java.security.Security.setProperty("ssl.TrustManagerFactory.algorithm",
+				"TrustAllCertificates");
+
+		Shibboleth shib = new Shibboleth("https://slcs1.arcs.org.au/SLCS/login");
+
+		PyInstance returnValue = shib.shibOpen(args[0], args[1].toCharArray(),
+				new StaticIdpObject("VPAC"));
+
+		Iterable<PyObject> it = returnValue.asIterable();
+
+		for (Iterator i = it.iterator(); i.hasNext();) {
+
+			System.out.println(i.next());
+
+		}
+
+		returnValue = shib.open();
+
+		it = returnValue.asIterable();
+
+		for (Iterator i = it.iterator(); i.hasNext();) {
+
+			System.out.println(i.next());
+
+		}
+
+	}
 
 }
