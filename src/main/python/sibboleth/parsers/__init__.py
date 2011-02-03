71b62bdd333461ade76c63db340ce752990e6703
Russell Sim <russell.sim@gmail.com>
2011-02-03 23:42:09 +1100

    reenabled lxml parser

--- src/main/python/sibboleth/parsers/__init__.py
+++ src/main/python/sibboleth/parsers/__init__.py
@@ -5,9 +5,9 @@ parsers = []
 from sibboleth.parsers.htmlparser import html_parser
 parsers.append(html_parser)
 
-#if not sys.platform.startswith('java'):
-#    from sibboleth.parsers.lxmlhtml import lxml_parser
-#    parsers.append(lxml_parser)
+if not sys.platform.startswith('java'):
+    from sibboleth.parsers.lxmlhtml import lxml_parser
+    parsers.append(lxml_parser)
 
 from sibboleth.parsers.soup import soup_parser
 parsers.append(soup_parser)
