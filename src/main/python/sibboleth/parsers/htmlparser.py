879dc80d6521bc4a8e81948f46034c8361b07750
Russell Sim <russell.sim@gmail.com>
2011-08-16 19:41:39 +1000

    escape data from the html parser, py2.5 comliance

--- src/main/python/sibboleth/parsers/htmlparser.py
+++ src/main/python/sibboleth/parsers/htmlparser.py
@@ -90,9 +90,49 @@ class FormParser(HTMLParser):
             self.in_ds = False
 
 
+# this was taken from the python.org wiki
+# http://wiki.python.org/moin/EscapingXml
+
+import xml.parsers.expat
+
+
+def unescape(s):
+    want_unicode = False
+    if isinstance(s, unicode):
+        s = s.encode("utf-8")
+        want_unicode = True
+
+    # the rest of this assumes that `s` is UTF-8
+    list = []
+
+    # create and initialize a parser object
+    p = xml.parsers.expat.ParserCreate("utf-8")
+    p.buffer_text = True
+    p.returns_unicode = want_unicode
+    p.CharacterDataHandler = list.append
+
+    # parse the data wrapped in a dummy element
+    # (needed so the "document" is well-formed)
+    p.Parse("<e>", 0)
+    p.Parse(s, 0)
+    p.Parse("</e>", 1)
+
+    # join the extracted strings and return
+    es = ""
+    if want_unicode:
+        es = u""
+    return es.join(list)
+
+# end wiki snippet
+
+
 def html_parser(html):
     parser = FormParser()
     for line in html:
         parser.feed(line)
     parser.close()
+    for form in parser.forms:
+        for attributes in form.values():
+            for attr in attributes:
+                attributes[attr] = unescape(attributes[attr])
     return None, parser.title, parser.forms
