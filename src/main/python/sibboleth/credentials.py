grith/sibboleth

commit 5dc82f3ae087157be2598cb83cd279537ac20fc3

Remove local idps attribute from Idp python class.
The Idp python class in sibboleth/credentials.py was defining its own set_idps
method that was assigning to the idps attribute - which was clashing with how
jython maps the set_idps method inherited from grixth.sibboleth.IdpObject.

Any attempts to instantiate the Idp python class were failing with

>>> import sibboleth.shibboleth
>>> idp = sibboleth.Idp("UoC")
Traceback (most recent call last):
  File "<stdin>", line 1, in <module>
  File "__pyclasspath__/sibboleth/credentials.py", line 125, in __init__
  File "__pyclasspath__/sibboleth/credentials.py", line 131, in set_idps
AttributeError: read-only attr: idps

It was still possible to instantiate grith.sibboleth.StaticIdpObject.

This commit removes the (otherwise unused) local attributes and accessor
methods from the python Idp class, deferring the handling to the methods
inherited from grixth.sibboleth.IdpObject.

src/main/python/sibboleth/credentials.py
@@ -121,14 +121,6 @@ class Idp(IIdp):
     """
     def __init__(self, idp=None):
         self.idp = idp or ''
-        self.raw_idps = {}
-        self.idps = []
-
-    def set_idps(self, idps):
-        """set the list of possible idps"""
-        self.raw_idps = idps
-        self.idps = idps.keys()
-        self.idps.sort()
 
     def prompt(self, controller):
         """some how decide what idp to authenticate to the

commit a24249ea7066880a8d432240f99f4e70905bb007

Define set_idps in IIdp in if not in jython
Fixes non-jython invocation of

python sibboleth/shiblogin.py

broken by 5dc82f3ae087157be2598cb83cd279537ac20fc3

src/main/python/sibboleth/credentials.py
@@ -31,7 +31,24 @@ if is_jython:
     from grith.sibboleth import IdpObject as IIdp
 else:
     ICredentialManager = object
-    IIdp = object
+
+    # declare an almost empty class IIdp - but we must define the set_idps method here
+    # (It was earlier declared in the IdP class itself, but that breaks under
+    # jython when it clashes with the set_idps method inhereted from Java
+    # IdpObject).  
+    # Hence, declare it into IIdp when NOT running under jython to provide an
+    # equivalent replacement.
+    ### IIdp = object
+    class IIdp:
+	def __init__(self):
+	    self.raw_idps = {}
+	    self.idps = []
+
+	def set_idps(self, idps):
+	    """set the list of possible idps"""
+	    self.raw_idps = idps
+	    self.idps = idps.keys()
+	    self.idps.sort()
 
 
 class AuthenticationException(Exception):